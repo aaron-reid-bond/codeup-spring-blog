f03856c32719590d1bed2cb1d27421f596d39412
Aaron Bond <aaron.reid.bond@gmail.com>
2023-08-09 07:43:58 -0700

    post db

--- src/main/java/com/codeup/codeupspringblog/controler/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controler/PostController.java
@@ -1,27 +1,29 @@
 package com.codeup.codeupspringblog.controler;
 
 import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
+    // These two next steps are often called dependency injection, where we create a Repository instance and initialize it in the controller class constructor.
+    private final PostRepository postDao;
+
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
+    }
+
     @GetMapping("/posts")
-    public String postsHome(Model model) {
-        ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post(1,"test1", "askdjfa;ksjdflskjdflskdjfsjfbnjksnfbjsn"));
-        posts.add(new Post(2,"test2", "askdjfa;ksjdflskjdflskdjfsjfbnjksnfbjsn"));
-        posts.add(new Post(3,"test3", "askdjfa;ksjdflskjdflskdjfsjfbnjksnfbjsn"));
-        model.addAttribute("posts", posts);
+    public String index(Model model) {
+        model.addAttribute("posts", postDao.findAll());
         return "posts/index";
     }
+
 
     @GetMapping("/posts/{id}")
     public String postId(@PathVariable String id, Model model) {
@@ -31,14 +33,13 @@ public class PostController {
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
     public String postsCreate() {
-        return "view the form for creating a post";
+        return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    @ResponseBody
-    public String postCreate() {
-        return "create a new post";
+    public String createPostPost(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
+        postDao.save(new Post(title, body));
+        return "redirect:/posts";
     }
 }
