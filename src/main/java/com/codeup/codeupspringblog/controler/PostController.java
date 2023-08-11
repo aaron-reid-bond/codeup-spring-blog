5e32536f10caea3c7bac1ba0048403d4289572a1
Aaron Bond <aaron.reid.bond@gmail.com>
2023-08-11 09:17:24 -0700

    email

--- src/main/java/com/codeup/codeupspringblog/controler/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controler/PostController.java
@@ -4,11 +4,10 @@ import com.codeup.codeupspringblog.models.Post;
 import com.codeup.codeupspringblog.models.User;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
+import com.codeup.codeupspringblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 
 @Controller
 public class PostController {
@@ -17,9 +16,12 @@ public class PostController {
 
     private final UserRepository userDao;
 
-    public PostController(PostRepository postDao, UserRepository userDao) {
+    private final EmailService emailService;
+
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
         this.postDao = postDao;
         this.userDao = userDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
@@ -51,7 +53,9 @@ public class PostController {
     @PostMapping("/posts/create")
     public String createPostPost(@ModelAttribute Post post){
         User user = userDao.getById(1L);
+        post.setUser(user);
         postDao.save(new Post(post));
+        emailService.prepareAndSend(post, user.getUsername() + " thanks for creating a post!", user.getUsername() + " thanks for creating the post: " + post.getTitle());
         return "redirect:/posts";
     }
 }
